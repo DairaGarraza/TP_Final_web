cb951aabc0dc4fe1db404352f81ecfe9c1d01c66
Ale <eze.giorgi@gmail.com>
2019-07-08 13:38:59 -0300

    Se muestra el color de fondo elegido

--- js/personalizacion.js
+++ js/personalizacion.js
@@ -9,4 +9,9 @@ $(document).ready(function(){
 			$("#tituloVistaPrevia").text($(this).val());
 		}		
 	});
+
+	$("input[name='fondo']").change(function(){
+		var color = $("input[name='fondo']:checked").val();
+		$(".vista-previa").css("background-color", color);		
+	});	
 });
