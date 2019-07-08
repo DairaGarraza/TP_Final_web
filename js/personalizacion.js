726be4400ac6709dc1bbfe7e8f9e5743f2839426
Ale <eze.giorgi@gmail.com>
2019-07-08 14:55:34 -0300

    Previsualizacion del tamanio de la fuente

--- js/personalizacion.js
+++ js/personalizacion.js
@@ -26,7 +26,12 @@ $(document).ready(function(){
 	$("input[name='color']").change(function(){
 		var colorFuente = $("input[name='color']:checked").val();
 		$(".vista-previa").css("color", colorFuente);		
-	});	
+	});
+
+	$("input[name='tamanioFuente']").change(function(){
+		var tamanioFuente = $("input[name='tamanioFuente']:checked").val();
+		$("#tituloVistaPrevia").css("font-size", tamanioFuente);
+	});
 
 	$("input[name='fondo']").change(function(){
 		var color = $("input[name='fondo']:checked").val();
