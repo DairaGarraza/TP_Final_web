6ddf21bc6da955d87f2471c5d9717c364651af2d
DairaGarraza <daira.garraza@gmail.com>
2019-07-09 23:32:15 -0300

    contacto responsive

---
+++ js/contacto.js
@@ -0,0 +1,30 @@
+ function validar(){
+	var error=false;
+	var mensajeError='';
+	var regextelefono = /^[0-9]{8}$/;
+	var regexemail = /^[0-9a-zA-Z._.-]+\@[0-9a-zA-Z._.-]+\.[0-9a-zA-Z]+$/;
+	var nombre=document.getElementById("nombre").value;
+	var email=document.getElementById("email").value;
+	var telefono=document.getElementById("telefono").value;
+
+	if(nombre==''){
+		error=true;
+		mensajeError+="<p>El nombre no puede estar vacío</p>";
+	}
+	if (!regexemail.test(email)) {
+		error=true;
+		mensajeError+="<p>Poner bien el email</p>";
+	}
+	
+	if( !regextelefono.test(telefono)){
+		error=true;
+		mensajeError+="<p>El telefono debe tener 8 dígitos</p>";
+	}
+
+	if(error==true){
+		document.getElementById("mensaje").innerHTML=mensajeError;
+		return false;
+	}else{
+		return true;
+	}
+}
